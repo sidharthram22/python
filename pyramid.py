a0db4e9ee3ee6ef5846e2fc2b8e1973d3e8fb5a8
sidharthram22 <sidsecond7@gmail.com>
2025-11-05 14:16:49 +0530

    Implement pyramid pattern in pyramid.py
    Add pyramid pattern printing based on user input.

---
+++ pyramid.py
@@ -0,0 +1,9 @@
+row=int(input("enter the row number :) "))
+for i in range(1,row + 1):
+ for j in range(i):
+    print("*",end="")
+    print()
+for j in range(row -1,0,-1):
+    for j in range (i):
+        print("*",end=" ")
+        print()
